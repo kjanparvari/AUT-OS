473185fbac378fdef7d386eeabd02809b438662a
kjanparvari <kjanparvari@gmail.com>
2020-05-10 10:22:13 -0700

    finally the user program is running the problem: qemu couldn't run on ubuntu 20.04 i installed ubuntu 18.04

---
+++ getChildrenTest.c
@@ -0,0 +1,15 @@
+//
+// Created by kjanparvari on 5/10/20.
+//
+
+#include "types.h"
+#include "stat.h"
+#include "user.h"
+
+
+int
+main(void)
+{
+    printf(1, "the number is: %d\n", getchildren());
+    exit();
+}
